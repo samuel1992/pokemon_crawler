0946b709df91e702e15f454e8ff76e5adcfcc860
samuel1992 <dantasamuca@gmail.com>
2023-07-13 00:40:31 +0200

    fix service insert condition and adjust unittests

--- src/pokemon/service.py
+++ src/pokemon/service.py
@@ -1,74 +1,72 @@
+from datetime import datetime
+from typing import List, Optional
+
 import sqlalchemy
 
-from typing import List
-from datetime import datetime
-
+from extensions import db
 from lib.pokemon_api import PokemonApi
 
-from extensions import db
-
-from .model import Pokemon, Ability
-from .dto import PokemonDTO, AbilityDTO
+from .dto import AbilityDTO, PokemonDTO
+from .model import Ability, Pokemon
+from .repository import AbilityRepository, PokemonRepository
+from .storage import PostgresStorage
 
 
 class PokemonService:
-    def __init__(self, db_session=None):
-        self._db = db_session or db
+    def __init__(
+        self,
+        pokemon_repository: Optional[PokemonRepository] = None,
+        ability_repository: Optional[AbilityRepository] = None
+    ):
+        self.pokemon_repository = pokemon_repository or PokemonRepository(
+            storage=PostgresStorage(),
+            entity=Pokemon
+        )
+        self.ability_repository = ability_repository or AbilityRepository(
+            storage=PostgresStorage(),
+            entity=Ability
+        )
+
+    def get_all_pokemons(self) -> List[PokemonDTO]:
+        return [PokemonDTO.from_instance(i) for i in self.pokemon_repository.get_all()]
 
     def pokemon_total(self):
-        return self._db.query(Pokemon).count()
+        return self.pokemon_repository.get_total()
 
     def ability_total(self):
-        return self._db.query(Ability).count()
+        return self.ability_repository.get_total()
 
-    def last_updated_pokemons(self, amount: int) -> List[int]:
-        query = self._db.query(Pokemon.id).order_by(
-            Pokemon.last_update.desc()
-        ).limit(amount)
-        return [i[0] for i in query.all()]
+    def last_updated_pokemons(self, amount: int) -> List[Optional[int]]:
+        result = self.pokemon_repository.get_all(amount)
+        if len(result) < 1:
+            return []
+
+        return [i.id for i in result]
 
     def fetch_new_pokemons(self):
         response = PokemonApi.get_all_pokemons()
-        pokemons = [PokemonDTO.from_dict(i).to_instance()
-                    for i in response]
-
+        pokemons = [PokemonDTO.from_dict(i) for i in response]
         for pokemon in pokemons:
-            try:
-                self._db.add(pokemon)
-                self._db.commit()
-            except sqlalchemy.exc.IntegrityError:
-                self._db.rollback()
-            finally:
-                self._db.close()
+            found = self.pokemon_repository.get_by_id(pokemon.id)
+            if found is None:
+                self.pokemon_repository.create(pokemon)
 
     def fetch_new_abilities(self, pokemon_id: int):
-        pokemon = self._db.query(Pokemon).get(pokemon_id)
-        pokemon.last_update = datetime.now()
-        self._db.add(pokemon)
-        self._db.commit()
+        pokemon_dto = self.pokemon_repository.get_by_id(pokemon_id)
+        pokemon_dto.last_update = datetime.now()
+        self.pokemon_repository.update(pokemon_dto)
 
-        response = PokemonApi.get_pokemon(pokemon.id)
+        response = PokemonApi.get_pokemon(pokemon_dto.id)
 
         abilities = []
         for a in response['abilities']:
-            ability = AbilityDTO.from_dict(
-                {**{'pokemon_id': pokemon.id}, **a['ability']}
-            ).to_instance()
-            abilities.append(ability)
+            abilities.append(AbilityDTO.from_dict(
+                {**{'pokemon_id': pokemon_dto.id}, **a['ability']}
+            ))
 
-        for ability in abilities:
-            try:
-                self._db.add(ability)
-                self._db.commit()
-            except sqlalchemy.exc.IntegrityError:
-                self._db.rollback()
-                self._db.query(Ability).filter(
-                    Ability.id == ability.id
-                ).update(dict(name=ability.name))
-                self._db.commit()
-            finally:
-                self._db.close()
-
-    def get_all_pokemons(self):
-        return [PokemonDTO.from_instance(i)
-                for i in self._db.query(Pokemon).all()]
+        for ability_dto in abilities:
+            found = self.ability_repository.get_by_name(ability_dto.name)
+            if found is None:
+                self.ability_repository.create(ability_dto)
+            else:
+                self.ability_repository.update(ability_dto)
--- src/pokemon/test_service.py
+++ src/pokemon/test_service.py
@@ -1,100 +1,136 @@
-import lib.pokemon_api
+from datetime import datetime
+from unittest import TestCase
+from unittest.mock import MagicMock, patch
 
-from unittest.mock import patch
-
-from fixtures import db
-
+from .dto import AbilityDTO, PokemonDTO
 from .service import PokemonService
-from .model import Pokemon, Ability
 
 
-@patch('lib.pokemon_api.PokemonApi.get_all_pokemons')
-def test_fetch_new_pokemons_and_save_it(mock_get_all_pokemons, db):
-    mock_get_all_pokemons.return_value = [
-        {
-            "name": "bulbasaur",
-            "url": "https://pokeapi.co/api/v2/pokemon/1/"
+class TestPokemonService(TestCase):
+    def setUp(self):
+        self.pokemon_repository = MagicMock()
+        self.ability_repository = MagicMock()
+        self.service = PokemonService(self.pokemon_repository, self.ability_repository)
+
+    @patch('lib.pokemon_api.PokemonApi.get_all_pokemons')
+    def test_fetch_new_pokemons_and_save_it(self, mock_get_all_pokemons):
+        pokemons_response = [
+            {
+                "name": "bulbasaur",
+                "url": "https://pokeapi.co/api/v2/pokemon/1/"
+            },
+            {
+                "name": "chupacabra",
+                "url": "https://pokeapi.co/api/v2/pokemon/2/"
+            }
+        ]
+        mock_get_all_pokemons.return_value = pokemons_response
+        self.pokemon_repository.get_by_id.return_value = None
+        self.pokemon_repository.create = MagicMock()
+
+        self.service.fetch_new_pokemons()
+
+        self.pokemon_repository.create.assert_called()
+        assert self.pokemon_repository.create.call_count == len(pokemons_response)
+        # Assert that the method create was called with an instance of PokemonDTO
+        for call_args in self.pokemon_repository.create.call_args_list:
+            for arg in call_args.args:
+                assert isinstance(arg, PokemonDTO)
+
+    @patch('lib.pokemon_api.PokemonApi.get_all_pokemons')
+    def test_fetch_pokemons_that_already_exist(self, mock_get_all_pokemons):
+        pokemons_response = [
+            {
+                "name": "bulbasaur",
+                "url": "https://pokeapi.co/api/v2/pokemon/1/"
+            },
+            {
+                "name": "chupacabra",
+                "url": "https://pokeapi.co/api/v2/pokemon/2/"
+            }
+        ]
+        mock_get_all_pokemons.return_value = pokemons_response
+        self.pokemon_repository.get_by_id.return_value = 1
+        self.pokemon_repository.create = MagicMock()
+
+        self.service.fetch_new_pokemons()
+
+        self.pokemon_repository.create.assert_not_called()
+
+    @patch('lib.pokemon_api.PokemonApi.get_pokemon')
+    def test_fetch_new_abilities_and_save_it(self, mock_get_pokemon):
+        now = datetime.now()
+        pokemon_dto = PokemonDTO(id=1, name='test', last_update=now)
+        abilities_response = {
+            "abilities": [
+                {
+                    "ability": {
+                        "name": "overgrow",
+                        "url": "https://pokeapi.co/api/v2/ability/65/"
+                    },
+                    "is_hidden": False,
+                    "slot": 1
+                }
+            ]
         }
-    ]
+        mock_get_pokemon.return_value = abilities_response
+        self.pokemon_repository.get_by_id.return_value = pokemon_dto
+        self.pokemon_repository.update = MagicMock()
+        self.ability_repository.get_by_name.return_value = None
 
-    PokemonService(db).fetch_new_pokemons()
+        self.service.fetch_new_abilities(1)
 
-    query = db.query(Pokemon)
+        assert pokemon_dto.last_update != now
 
-    assert query.count() == 1
-    assert query.first().name == 'bulbasaur'
-    assert query.first().id == 1
+        self.ability_repository.create.assert_called()
+        assert self.ability_repository.create.call_count == len(abilities_response['abilities'])
+        # Assert that the method create was called with an instance of AbilityDTO
+        for call_args in self.ability_repository.create.call_args_list:
+            for arg in call_args.args:
+                assert isinstance(arg, AbilityDTO)
 
+    @patch('lib.pokemon_api.PokemonApi.get_pokemon')
+    def test_fetch_already_existent_ability_but_updates_it(self, mock_get_pokemon):
+        now = datetime.now()
+        pokemon_dto = PokemonDTO(id=1, name='test', last_update=now)
+        abilities_response = {
+            "abilities": [
+                {
+                    "ability": {
+                        "name": "test 2",
+                        "url": "https://pokeapi.co/api/v2/ability/1/"
+                    },
+                    "is_hidden": False,
+                    "slot": 1
+                }
+            ]
+        }
+        ability_dto = AbilityDTO.from_dict({
+            **{'pokemon_id': pokemon_dto.id},
+            **abilities_response['abilities'][0]['ability']
+        })
+        mock_get_pokemon.return_value = abilities_response
+        self.pokemon_repository.get_by_id.return_value = pokemon_dto
+        self.pokemon_repository.update = MagicMock()
+        self.ability_repository.get_by_name.return_value = ability_dto
 
-@patch('lib.pokemon_api.PokemonApi.get_pokemon')
-def test_fetch_new_abilities_and_save_it(mock_get_pokemon, db):
-    pokemon = Pokemon(id=1, name='bulbasaur')
-    db.add(pokemon)
-    db.commit()
+        self.service.fetch_new_abilities(1)
 
-    mock_get_pokemon.return_value = {
-        "abilities": [
-            {
-                "ability": {
-                    "name": "overgrow",
-                    "url": "https://pokeapi.co/api/v2/ability/65/"
-                },
-                "is_hidden": False,
-                "slot": 1
-            }
-        ]
-    }
+        assert pokemon_dto.last_update != now
 
-    assert pokemon.last_update is None
+        self.ability_repository.update.assert_called()
+        # Assert that the method update was called with an instance of AbilityDTO
+        for call_args in self.ability_repository.update.call_args_list:
+            for arg in call_args.args:
+                assert isinstance(arg, AbilityDTO)
 
-    PokemonService(db).fetch_new_abilities(pokemon.id)
-    pokemon = db.query(Pokemon).first()
+    def test_get_last_updated_pokemons(self):
+        pokemon_dto = PokemonDTO(id=1, name='test')
+        self.pokemon_repository.get_all.return_value = [pokemon_dto]
 
-    assert pokemon.last_update is not None
+        ids = self.service.last_updated_pokemons(1)
 
-    ability = pokemon.abilities[0]
+        assert len(ids) == 1
+        assert ids[0] == 1
 
-    assert ability.name == 'overgrow'
-    assert ability.id == 65
-
-
-@patch('lib.pokemon_api.PokemonApi.get_pokemon')
-def test_fetch_already_existent_ability_but_updates_it(mock_get_pokemon, db):
-    pokemon = Pokemon(id=1, name='bulbasaur')
-    ability = Ability(id=1, pokemon_id=pokemon.id, name='test 1')
-    pokemon.add_abilities([ability])
-    db.add(pokemon)
-    db.commit()
-
-    mock_get_pokemon.return_value = {
-        "abilities": [
-            {
-                "ability": {
-                    "name": "test 2",
-                    "url": f"https://pokeapi.co/api/v2/ability/{ability.id}/"
-                },
-                "is_hidden": False,
-                "slot": 1
-            }
-        ]
-    }
-
-    assert ability.name == 'test 1'
-
-    PokemonService(db).fetch_new_abilities(pokemon.id)
-
-    pokemon = db.query(Pokemon).first()
-    ability = pokemon.abilities[0]
-
-    assert ability.name == 'test 2'
-
-
-def test_get_last_updated_pokemons(db):
-    pokemon = Pokemon(id=1, name='bulbasaur')
-    db.add(pokemon)
-    db.commit()
-
-    ids = PokemonService(db).last_updated_pokemons(1)
-
-    assert len(ids) == 1
-    assert ids[0] == 1
+        self.pokemon_repository.get_all.assert_called_with(1)
